bce9530df566155773c9d4fd5ad16465c4f730a7
ShaikAhamed08 <147649437+ShaikAhamed08@users.noreply.github.com>
2024-01-02 13:36:24 +0530

    e_commerce_query.sql

--- e_commerce_query.sql
+++ e_commerce_query.sql
@@ -7,11 +7,7 @@ left join user
 on user.userid=address.userId 
 where user.userId=5;
 
--- 2)Query the information of laptop
-
-
-
--- 3)Query the total quantity of products from store with storeid 8 in the shopping cart?
+-- 2)Query the total quantity of products from store with storeid 8 in the shopping cart?
 select product.name,sum(quantity) from product
 join store 
 on product.s_id=store.s_id
@@ -20,7 +16,7 @@ on save_to_shopping_cart.p_id=product.p_id
 where store.s_id='8'
 group by product.name;
 
--- 4)Query the name and address of orders delivered on 2017-02-17?
+-- 3)Query the name and address of orders delivered on 2017-02-17?
 select name,streetAddr from address 
  inner join Deliver_to 
 on address.addr_id=Deliver_to.addr_id
@@ -28,36 +24,34 @@ left join orders
 on orders.ordernumber=deliver_to.ordernumber
 where timedelivered='2017-02-17';
 
--- 5)Query the comments of product 12345678?
-
--- 6)Insert the user id of sellers whose name starts with A into buyer?
+-- 4)Insert the user id of sellers whose name starts with A into buyer?
 insert into buyer 
 (select user.userId from user
 inner join seller
 on user.userId=seller.userId
 where user.name regexp '^a');
 
--- 7) Update the payment state of orders to unpaid which created after year 2017 and with total amount greater than 50.?
+-- 5) Update the payment state of orders to unpaid which created after year 2017 and with total amount greater than 50.?
 
 update orders
 set paymentStatus='Unpaid'
 where totalAmount > 50 and year(creationtime)>=2017;
 
--- 8)Update the name and contact phone number of address where the provice is Quebec and city is montreal.?
+-- 6)Update the name and contact phone number of address where the provice is Quebec and city is montreal.?
 update address
 set  name= 'Vito Carleone',phone_number='824-977-2892'
 where province='Quebec' and city='montreal';
 
 
 
--- 9)Create view of all products whose price above average price?
+-- 7)Create view of all products whose price above average price?
 
 
 create view products_average as
 select * from product
 where price > (select avg(price) as avgg from product);
 
--- 10)Create view of all products sales in 2016.?
+-- 8)Create view of all products sales in 2016.?
 
 create view products_2016 as
 select * from product 
